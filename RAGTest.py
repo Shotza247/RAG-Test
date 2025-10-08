0d761f81775f95dd3079af7a6316706d7d7fc803
Jabulani Ndlovu <jabulani.ndlovu360@gmail.com>
2025-10-08 12:00:38 +0200

    Removed all the unnecessary comments that were useful during testing.

--- RAGTest.py
+++ RAGTest.py
@@ -2,12 +2,6 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 
-#Look at the first page
-#print("FIRST PAGE CONTENT")
-#print("=" * 50)
-#print(pages[0].page_content)
-#print("\n")
-
 def pagestester(pages):
     #See what information is on the 1st page
     print("FIRST PAGE METADATA")
@@ -41,27 +35,6 @@ def pagestester(pages):
             print(pages[page_number].page_content[:500]) 
             print("...\n")
 
-#Exercises
-#Exercise 1: Print the last page
-#print(pages[-1].page_content)
-
-#Exercise 2: Count total characters in all pages
-#total_chars = sum(len(page.page_content) for page in pages)
-#print(f"Total characters in document: {total_chars}")
-
-#Exercise 3: Find which page mentions "MongoDB"
-
-#print("SEARCHING FOR 'MongoDB' IN ALL PAGES")
-#print("=" * 50)
-
-#pageholder = " "
-
-#for i, page in enumerate(pages):
-#    if "MongoDB" in page.page_content:
-#        results = f"MongoDB mentioned on page {i + 1}"
-#    else:
-#        results = pageholder + f"{i + 1},"
-#    print(results)
 
 # Exercise 4: Save first page to a text file
 # with open("first_page.txt", "w", encoding="utf-8") as f:
